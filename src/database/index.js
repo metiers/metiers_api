03d538a18106cfdf8460a78cbd56de9635586c40
Jamie <jamie.lo112@gmail.com>
2017-12-26 17:21:40 -0800

    [add]
    set up basic server/routes/db schema and initialize db

--- src/database/index.js
+++ src/database/index.js
@@ -1,1 +1,20 @@
-//scaffold
+import mysql from 'mysql2';
+import { createDb } from './init';
+export let db;  
+
+export const initiateDb = () => {
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    multipleStatements: true,
+  });
+  connection.connect((err) => {
+    if (err) {
+      throw err;
+    } else {
+      db = connection;
+      createDb();
+    }
+  });
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,19 @@
-//scaffold
+import express from 'express';
+
+const router = express.Router();
+
+router.route('/signup')
+
+router.route('/login')
+
+router.route('/github')
+
+router.route('/dashboard')
+
+router.route('/manual')
+
+router.route('/search')
+
+router.route('/jobDetail')
+
+export default router;
