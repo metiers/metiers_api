31fe5f57bfaf1467237e4bb149525026edcfc192
Jamie <jamie.lo112@gmail.com>
2018-01-01 17:05:29 -0800

    [add]
    now post job creation to history table in DB

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -16,13 +16,18 @@ export default {
       data.job.userId = parseInt(data.id);
       data.job.companyId = company.insertId;
       sql = helper.insert('job', data.job);
-      db.query(sql, (err, data) => {
-        if (err) {
-          res.status(400).send('Cannot insert into DB');
-        } else {
-          res.status(200).send(data);
-        }
+      db.query(sql, (err, job) => {
+        if (err) throw err;
+
+        db.query(`INSERT INTO history (jobId, name, timeStamp) VALUES (${job.insertId}, 'Created job', CURRENT_TIMESTAMP());`, (err, data) => {
+          if (err) {
+            res.status(400).send('Cannot insert into DB');
+          } else {
+            res.status(200).send(data);
+          }
+        })
       })
+
     })
   }
 }
